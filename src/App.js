450887f205a0782f1748817cb8341f2ff034728b
Joseph Black <josephconradblack@gmail.com>
2018-06-30 13:52:33 +1000

    Setup theme provider and initial page container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import CombatPlannerContainer from './containers/CombatPlannerContainer';
+
+import theme from './theme';
 
 class App extends Component {
   render() {
-    return <div>App Container</div>;
+    return (
+      <div>
+        <ThemeProvider theme={theme}>
+          <CombatPlannerContainer />
+        </ThemeProvider>
+      </div>
+    );
   }
 }
 
