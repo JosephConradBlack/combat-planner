e4dd810ab942505f7dc8283d3c5210958f418290
Joseph Black <josephconradblack@gmail.com>
2018-07-03 21:19:25 +1000

    Add initial state handling to FighterEditableForm

--- src/components/FighterEditableForm.js
+++ src/components/FighterEditableForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import {
@@ -18,65 +18,118 @@ const StyledIcon = styled(Icon)`
   }
 `;
 
-const FighterEditableForm = () => {
-  return (
-    <Segment>
-      <Form>
-        <Header as="h2">Add fighter</Header>
-        <Form.Input
-          required
-          label="Fighter Name"
-          placeholder="Goblin #1"
-          name="name"
-        />
-        <Form.Group widths="equal">
-          <Form.Field
-            type="number"
-            control="input"
-            width={4}
-            label="Initiative"
-            placeholder="17"
-            name="initiative"
-          />
+class FighterEditableForm extends Component {
+  constructor(props) {
+    super(props);
 
-          <Responsive
-            as={StyledIcon}
-            minWidth={768}
-            size="big"
-            name="heart outline"
-            color="red"
-          />
-          <Form.Field
-            type="number"
-            control="input"
-            label="Health"
-            placeholder="30"
-            name="health"
-          />
+    this.state = {
+      name: '',
+      initiative: '',
+      health: '',
+      armor: '',
+      player: false
+    };
+  }
 
-          <Responsive
-            as={StyledIcon}
-            minWidth={768}
-            size="big"
-            name="shield"
-            color="grey"
+  handleChange = (e, target) => {
+    const { name, value, checked, type } = target ? target : e.target;
+
+    if (type && type == 'checkbox') {
+      this.setState(Object.assign({}, this.state, { [name]: checked }));
+    } else {
+      this.setState(Object.assign({}, this.state, { [name]: value }));
+    }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+  };
+
+  handleCancel = e => {
+    e.preventDefault();
+
+    this.setState({
+      name: '',
+      initiative: '',
+      health: '',
+      armor: '',
+      player: ''
+    });
+  };
+
+  render() {
+    return (
+      <Segment>
+        <Form>
+          <Header as="h2">Add fighter</Header>
+          <Form.Input
+            required
+            label="Fighter Name"
+            placeholder="Goblin #1"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
           />
-          <Form.Field
-            type="number"
-            control="input"
-            label="Armor"
-            placeholder="18"
-            name="armor"
+          <Form.Group widths="equal">
+            <Form.Field
+              type="number"
+              control="input"
+              width={4}
+              label="Initiative"
+              placeholder="17"
+              name="initiative"
+              value={this.state.initiative}
+              onChange={this.handleChange}
+            />
+
+            <Responsive
+              as={StyledIcon}
+              minWidth={768}
+              size="big"
+              name="heart outline"
+              color="red"
+            />
+            <Form.Field
+              type="number"
+              control="input"
+              label="Health"
+              placeholder="30"
+              name="health"
+              value={this.state.health}
+              onChange={this.handleChange}
+            />
+
+            <Responsive
+              as={StyledIcon}
+              minWidth={768}
+              size="big"
+              name="shield"
+              color="grey"
+            />
+            <Form.Field
+              type="number"
+              control="input"
+              label="Armor"
+              placeholder="18"
+              name="armor"
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Checkbox
+            label="Is this a player?"
+            name="player"
+            checked={this.state.player}
+            onChange={this.handleChange}
           />
-        </Form.Group>
-        <Checkbox label="Is this a player?" name="player" />
-        <Divider />
-        <Button basic content="Cancel" />
-        <Button positive floated="right" content="Add" />
-      </Form>
-    </Segment>
-  );
-};
+          <Divider />
+          <Button basic content="Cancel" />
+          <Button positive floated="right" content="Add" />
+        </Form>
+      </Segment>
+    );
+  }
+}
 
 FighterEditableForm.propTypes = {};
 
