020c16ffdefac471bb7dddf56aeb854f7e09d558
Joseph Black <josephconradblack@gmail.com>
2018-07-21 19:02:59 +1000

    Add redux-persist to store state into local storage

--- src/index.js
+++ src/index.js
@@ -8,14 +8,31 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import autoMergeLevel1 from 'redux-persist/lib/stateReconciler/autoMergeLevel1';
+import localForage from 'localforage';
+
+const persistConfig = {
+  key: 'root',
+  storage: localForage,
+  stateReconciler: autoMergeLevel1
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
